9e2933784dc25c514f46ae80439531f221e0a199
Tên của bạn <email@gmail.com>
2026-04-05 10:10:22 +0700

    2

--- cogs/ban.py
+++ cogs/ban.py
@@ -12,7 +12,7 @@ _MESSAGEABLE_GUILD = (discord.TextChannel, discord.Thread, discord.VoiceChannel)
 # --- Cấu hình — chỉnh trực tiếp trong file này ---
 # Khớp không phân biệt hoa thường (substring trong tin + chữ trong embed). List rỗng = tắt lọc.
 BANNED_WORDS: list[str] = [
-    "testbot11","nhìn lại mình đi","anh em out hết còn gì trong tay","BHHAHAHA","yoooooo watch the girl in vc BHHAHAHA","yoooooo watch","bot ngu","bot dốt","discordapp.com/invite/","bot đần","chí momo","chó mimi","https://discord.com/channels/1184348724999225355/1411448689192603669",
+    "testbot11","nhìn lại mình đi","anh em out hết còn gì trong tay","BHHAHAHA","yoooooo watch the girl in vc BHHAHAHA","yoooooo watch","discordapp.com/invite/","chí momo","chó mimi","https://discord.com/channels/1184348724999225355/1411448689192603669",
     
    
 ]
